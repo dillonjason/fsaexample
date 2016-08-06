09e85d70807a666a9c48883bcf34c3f935c2a7f9
dillonjason <djasuar@gmail.com>
2016-08-06 18:56:50 -0400

    Depts directory styled

--- app/views/home.jsx
+++ app/views/home.jsx
@@ -6,7 +6,21 @@ class Home extends React.Component {
         return (
             <div className="home-container">
                 <SearchBar />
-                <DeptDirectory />
+
+                <div className="pure-g">
+                    <div className="pure-u-1-6">
+                        <DeptDirectory />
+                    </div>
+
+                    <div className="pure-u-1-2 general-margin-left">
+                        <h2>Home</h2>
+                    </div>
+
+                    <div className="pure-1-4">
+
+                    </div>
+                </div>
+
             </div>
         );
     }
